7fae1d3eee09114e05aad23e8e07b8cc4272d104
Melissa <melisadamian@yahoo.com>
2018-03-02 00:43:51 +0200

    serverul accepta oricate mesaje de la toti clientii

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-// Server side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -15,22 +14,17 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
     char *hello = "Hello from server";
-      
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-      
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-      
+    address.sin_port = htons(PORT); 
     // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -40,36 +34,26 @@ int main(int argc, char const *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
     for (;;)
     {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                        (socklen_t*)&addrlen))<0)
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("accept");
             exit(EXIT_FAILURE);
         } 
 
         if (fork()==0)  /* procesul fiu */
-    { 
-        close(server_fd);
-        /*procesare cerere*/
-        valread = read( new_socket , buffer, 1024);
-        printf("%d\n",valread );
-        send(new_socket , (char*)buffer , strlen(buffer) , 0 );
-        printf("Message sent\n");
-  
-        printf("IP address is: %s %s\n",inet_ntoa(address.sin_addr), buffer );
-        close (new_socket);
-        exit(0);
-    }
-    
-        //close (new_socket);  /* parinte */
+        { 
+	    close(server_fd);
+	    //procesare cerere
+	    while((valread = read(new_socket, buffer, 1024))>0)
+	    {
+		send(new_socket, (char*)buffer, sizeof(buffer), 0);
+		printf("IP address is: %s %s\n",inet_ntoa(address.sin_addr), buffer);
+	    }
+	    close (new_socket);
+	    exit(0);
         }
-    }
+        //close (new_socket);  /* parinte */
+     }
+}
