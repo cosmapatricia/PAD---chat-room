cd324e5912e58ddce59cbf8df516a5d6672fc38d
Patricia <cosma_patricia@yahoo.com>
2018-03-18 21:43:28 +0200

    send size, then message

---
+++ client_thread_var5.c
@@ -0,0 +1,178 @@
+#include<stdio.h> 
+#include<string.h>    
+#include<sys/socket.h>    
+#include<arpa/inet.h> 
+#include<unistd.h>
+#include<stdlib.h>
+#include<pthread.h>        //for threading, compile with -pthread
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void end_transmission(int sign)
+{
+    //printf("End of transmission.\n");
+    exit(0);
+}
+
+int main(int argc , char *argv[])
+{
+    int sock, *new_sock;
+    struct sockaddr_in server;
+    //Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        printf("Error creating socket\n");
+	exit(1);
+    }
+    //puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8888);
+
+    //Connect to remote server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) ==-1)
+    {
+        printf("Error connecting socket\n");
+        exit(2);
+    }
+    puts("Connected");
+
+    pid_t pid_transmit, pid_receive;
+    pid_transmit=fork();
+    if(pid_transmit == -1)
+    {
+    	printf("Error creating child process\n");
+        exit(3);
+    }
+    if(pid_transmit == 0)            
+    {
+        struct sigaction act;
+        act.sa_handler=end_transmission;
+        act.sa_flags=0;
+        if(sigaction(SIGUSR1, &act, NULL)<0)
+        {
+            printf("Error setting the handler for SIGUSR1\n");
+            exit(4);
+        }
+        
+        char username[512];
+	char password[512];  
+        
+        printf("Enter your username: ");
+        fgets(username,sizeof(username),stdin);
+        strtok(username,"\n");		                             //remove newline from string
+
+	printf("Enter your password: ");
+	fgets(password,sizeof(password),stdin);
+	strtok(password,"\n");
+
+                        
+        char message[1025];
+        strcpy(message,username);  
+	strcat(message,":");      
+        strcat(message,password);
+        
+        int size;
+        size=strlen(message)+1;        // +1 pt terminatorul de sir
+        
+        //printf("login size: %d\n", size);
+        
+        if(send(sock, &size, sizeof(size), 0) == -1)
+        {
+              printf("Send login size failed\n");
+              exit(5);
+        }
+        
+        //Send login info
+        if(send(sock, message, size, 0) == -1)
+        {
+              printf("Send login info failed\n");
+              exit(6);
+        }
+        
+        
+        while(1) //while(fgets)
+        {
+            fgets(message, sizeof(message), stdin);
+            strtok(message,"\n"); 			
+                
+            char aux[1024];
+            strcpy(aux, username);
+            strcat(aux, ": ");
+            strcat(aux, message);
+            strcpy(message, aux);
+            size=strlen(message)+1;   // +1 pt terminatorul de sir
+                
+            //printf("message size: %d\n", size);
+               
+            if(send(sock, &size, sizeof(size), 0) == -1)
+            {
+                printf("Send message size failed\n");
+                exit(7);
+            }
+                
+            //Send some data
+            if(send(sock, message, size, 0) == -1)
+            {
+                printf("Send message failed\n");
+                exit(8);
+            }
+        }
+
+        close(sock);
+        exit(0);
+    }
+            
+    pid_receive=fork();
+    if(pid_receive == -1)
+    {
+        printf("Error creating child process\n");
+        exit(9);
+    }
+    if(pid_receive == 0)
+    {
+        char server_reply[1025];
+        int green_light;
+        int size;
+        
+        if(recv(sock, &green_light, sizeof(green_light), 0) > 0  && green_light==1)
+        {
+            //printf("green_light: %d\n", green_light);
+        }
+        else
+        {            
+            printf("Error receiving the green light to send login info\n");
+            exit(10);
+        }
+        
+        
+        while(recv(sock, &size, sizeof(size), 0) > 0  && recv(sock, server_reply, size, 0) > 0)
+        {
+	     if(strcmp(server_reply,"Wrong password!")==0)
+	     {
+                 printf("%s\n", server_reply);
+                 if(kill(pid_transmit, SIGUSR1)<0)
+                 {
+                    printf("Error sending SIGUSR1 to the transmission process\n");
+                    exit(11);
+                 }
+		 close(sock);
+		 exit(0);
+	     }
+	     //printf("size of message received: %d\n", size);
+             printf("%s\n", server_reply);
+        }      
+        close(sock);                
+        exit(0);
+    }
+           
+    int status1, status2;
+    wait(&status1);
+    wait(&status2);
+
+    close(sock);
+    return 0;
+}
+
